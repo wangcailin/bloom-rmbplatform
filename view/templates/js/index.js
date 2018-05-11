4e3680eb19244abaf8310a8067aba96a8c4ecba7
耿旭亮 <416542094@qq.com>
2018-05-11 17:03:07 +0800

    debug

--- view/templates/js/index.js
+++ view/templates/js/index.js
@@ -206,10 +206,7 @@ $(function(){
 		capture: false,
 		passive: false
 	} : false);
-    document.getElementsByClassName('register')[0].addEventListener('touchmove', function (e) { e.preventDefault(); }, isPassive() ? {
-        capture: false,
-        passive: false
-    } : false);
+   
 	function isPassive() {
 	    var supportsPassiveOption = false;
 	    try {
