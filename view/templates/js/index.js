fae9f3e915ed0c9e0f92dfe27364ec5eeccb69c6
耿旭亮 <416542094@qq.com>
2018-05-11 15:10:46 +0800

    debug

--- view/templates/js/index.js
+++ view/templates/js/index.js
@@ -177,20 +177,20 @@ $(function(){
          },0);
       }
    })
-	// document.addEventListener('touchmove', function (e) { e.preventDefault(); }, isPassive() ? {
-	// 	capture: false,
-	// 	passive: false
-	// } : false);
-	// function isPassive() {
-	//     var supportsPassiveOption = false;
-	//     try {
-	//         addEventListener("test", null, Object.defineProperty({}, 'passive', {
-	//             get: function () {
-	//                 supportsPassiveOption = true;
-	//             }
-	//         }));
-	//     } catch(e) {}
-	//     return supportsPassiveOption;
-	// }
+	document.getElementById('maskScroll').addEventListener('touchmove', function (e) { e.preventDefault(); }, isPassive() ? {
+		capture: false,
+		passive: false
+	} : false);
+	function isPassive() {
+	    var supportsPassiveOption = false;
+	    try {
+	        addEventListener("test", null, Object.defineProperty({}, 'passive', {
+	            get: function () {
+	                supportsPassiveOption = true;
+	            }
+	        }));
+	    } catch(e) {}
+	    return supportsPassiveOption;
+	}
 
 })
